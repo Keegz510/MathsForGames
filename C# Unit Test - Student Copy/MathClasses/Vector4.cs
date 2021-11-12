39669fb1c5a65a1502dfb955e316cf078e9a9253
Keegan <KMiller.EilexInteractive@gmail.com>
2021-11-12 19:45:33 +1100

    Added multiplaction for the vetor4s

--- C# Unit Test - Student Copy/MathClasses/Vector4.cs
+++ C# Unit Test - Student Copy/MathClasses/Vector4.cs
@@ -39,7 +39,7 @@ namespace MathClasses
             );
         }
 
-        // Addition of 2 Vector4s
+        // subtraction of 2 Vector4s
         public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
         {
             return new Vector4
@@ -48,6 +48,18 @@ namespace MathClasses
                 lhs.y - rhs.y,
                 lhs.z - rhs.z,
                 lhs.w - rhs.w
+            );
+        }
+
+        // Multiplication of 2 Vector4s
+        public static Vector4 operator *(Vector4 lhs, Vector4 rhs)
+        {
+            return new Vector4
+            (
+                lhs.x * rhs.x,
+                lhs.y * rhs.y,
+                lhs.z * rhs.z,
+                lhs.w * rhs.w
             );
         }
 
