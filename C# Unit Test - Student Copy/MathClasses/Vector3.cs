188d74d5bbd4b39e6a95443ca487d7629dbfb75a
Keegan <KMiller.EilexInteractive@gmail.com>
2021-11-12 19:25:39 +1100

    Added addition operator to the vector3

--- C# Unit Test - Student Copy/MathClasses/Vector3.cs
+++ C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -8,13 +8,31 @@ namespace MathClasses
 {
     public class Vector3
     {
-        public float x, y, z;
+        public float x, y, z;           // Vector Values
 
         public Vector3()
         {
             x = 0;
             y = 0;
             z = 0;
+        }
+        // Constructor for floats
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // Addition of 2 vectors
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3
+            (
+                lhs.x + rhs.x,
+                lhs.y + rhs.y,
+                lhs.z + rhs.z
+            );
         }
 
     }
