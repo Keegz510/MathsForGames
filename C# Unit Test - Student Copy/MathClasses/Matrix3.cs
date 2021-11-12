1988a9e336405cc3068c8f8422a9969085fafbc4
Keegan <KMiller.EilexInteractive@gmail.com>
2021-11-13 00:57:07 +1100

    Added constructors for the matrix3

--- C# Unit Test - Student Copy/MathClasses/Matrix3.cs
+++ C# Unit Test - Student Copy/MathClasses/Matrix3.cs
@@ -8,9 +8,18 @@ namespace MathClasses
 {
     public class Matrix3
     {
-        public float[] m = new float[9];                // Matrix array
+        public float[] matrix = new float[9];                // Matrix array
 
-        public float m1 {  get => m[0]f}
+        public float m1 { get => matrix[0]; set => matrix[0] = value; }
+        public float m2 { get => matrix[0]; set => matrix[0] = value; }
+        public float m3 { get => matrix[0]; set => matrix[0] = value; }
+        public float m4 { get => matrix[0]; set => matrix[0] = value; }
+        public float m5 { get => matrix[0]; set => matrix[0] = value; }
+        public float m6 { get => matrix[0]; set => matrix[0] = value; }
+        public float m7 { get => matrix[0]; set => matrix[0] = value; }
+        public float m8 { get => matrix[0]; set => matrix[0] = value; }
+        public float m9 { get => matrix[0]; set => matrix[0] = value; }
+
 
         public Matrix3()
         {
@@ -18,6 +27,20 @@ namespace MathClasses
             m4 = 0; m5 = 1; m6 = 0;
             m7 = 0; m8 = 0; m9 = 1;
         }
+
+        public Matrix3(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m9)
+        {
+            this.m1 = m1; this.m2 = m2; this.m3 = m3;
+            this.m4 = m4; this.m5 = m5; this.m6 = m6;
+            this.m7 = m7; this.m8 = m8; this.m9 = m9;
+        }
+
+        public Matrix3(float[] m)
+        {
+            matrix = m;
+        }
+
+        
 
     }
 }
