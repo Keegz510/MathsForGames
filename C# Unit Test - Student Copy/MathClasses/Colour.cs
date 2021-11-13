651a9a22fd4891c23bf528a62d4da66be678fc19
Keegan <KMiller.EilexInteractive@gmail.com>
2021-11-13 17:58:33 +1100

    Added bitmask for the colors

--- C# Unit Test - Student Copy/MathClasses/Colour.cs
+++ C# Unit Test - Student Copy/MathClasses/Colour.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace MathClasses
 {
+    [Flags]
+    public enum Bitmask : uint
+    {
+        RED = 4278190080,
+        GREEN = 16711680,
+        BLUE = 65280,
+        ALPHA = 255
+    }
+
     public class Colour
     {
         public UInt32 colour;
