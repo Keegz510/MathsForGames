41d278b5e6e8f4cafb7bd0091754d437d87a4652
Keegz510 <s200322@students.aie.edu.au>
2021-12-03 13:27:01 +1100

    Added Base constructors with parent & children objects

--- C# Unit Test - Student Copy/TankGame/SceneObject.cs
+++ C# Unit Test - Student Copy/TankGame/SceneObject.cs
@@ -10,7 +10,54 @@ namespace TankGame
 {
     public class SceneObject
     {
-        
+        protected SceneObject parent;
+        public SceneObject Parent { get => parent; }
+
+        protected List<SceneObject> children = new List<SceneObject>();
+        public List<SceneObject> Children { get => children; }
+
+        public SceneObject()
+        {
+
+        }
+
+        public SceneObject(SceneObject parent)
+        {
+            this.parent = parent;
+        }
+
+        public SceneObject(SceneObject parent, List<SceneObject> children)
+        {
+            this.parent = parent;
+            this.children = children;
+        }
+
+        public void SetParent(SceneObject newParent)
+        {
+            if(parent != null)
+            {
+                parent.RemoveChild(this);
+            }
+
+            parent = newParent;
+            parent.AddChild(this);
+        }
+
+        public void AddChild(SceneObject newChild)
+        {
+            if(!children.Contains(newChild))
+            {
+                children.Add(newChild);
+            }
+        }
+
+        public void RemoveChild(SceneObject child)
+        {
+            if(children.Contains(child))
+            {
+                children.Remove(child);
+            }
+        }
 
     }
 }
