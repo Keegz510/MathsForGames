84de9956861cbffe604eb56726b4b597bba6aaa4
Keegz510 <s200322@students.aie.edu.au>
2021-12-08 12:39:13 +1100

    Created global class to store global properties and update methods

---
+++ C# Unit Test - Student Copy/TankGame/Globals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClasses;
+
+namespace TankGame
+{
+    public static class Globals
+    {
+        public static float DeltaTime;
+
+        public static SceneObject Scene;            /// ROOT SCENE OBJECT
+        public static List<SceneObject> AllObjectsInScene = new List<SceneObject>();
+
+        //Returns the 2D rotation matrix of a given angle
+        public static Matrix3 RotationMatrix2D(float v)
+        {
+            return new Matrix3(
+                (float)Math.Cos(-v), (float)-Math.Sin(-v), 0,
+                (float)Math.Sin(-v), (float)Math.Cos(-v), 0,
+                 0, 0, 1);
+        }
+    }
+}
