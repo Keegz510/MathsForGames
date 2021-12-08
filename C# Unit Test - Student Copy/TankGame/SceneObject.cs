6c05dd1e3c7a584c3a31505f6ffb2935ad1e851c
Keegz510 <s200322@students.aie.edu.au>
2021-12-08 11:58:25 +1100

    Added hierachy to the scene object

--- C# Unit Test - Student Copy/TankGame/SceneObject.cs
+++ C# Unit Test - Student Copy/TankGame/SceneObject.cs
@@ -10,94 +10,24 @@ namespace TankGame
 {
     public class SceneObject
     {
-        #region Parenting
-        /// Reference to the parent object of this obejct
-        protected SceneObject parent;
-        /// returns the parent object
-        public SceneObject Parent { get => parent; }
-        /// List of children linked to this object
-        protected List<SceneObject> children = new List<SceneObject>();
-        /// Returns the children of this ohbject
-        public List<SceneObject> Children { get => children; }
-        #endregion
+        public SceneObject Parent
+        {
+            get => Parent;
+            set
+            {
+                if (Parent != null)
+                    Parent.Children.Remove(this);
 
-        #region Object Transform
+                Parent = value;
+                Parent.Children.Add(this);
+            }
+        }
+        public List<SceneObject> Children = new List<SceneObject>();
 
-        /// Reference to the local transform
-        protected Matrix3 localTransform = new Matrix3();
-        public Matrix3 LocalTransform { get => localTransform;  }
-        /// Reference to the global transform
-        protected Matrix3 globalTransform = new Matrix3();
-        public Matrix3 GlobalTransform { get => globalTransform; }
+        
 
-        #endregion
 
-        /// <summary>
-        /// Default constructor
-        /// </summary>
         public SceneObject()
-        {
-
-        }
-
-        /// <summary>
-        /// Constructor with parent
-        /// </summary>
-        /// <param name="parent">Parent of this object</param>
-        public SceneObject(SceneObject parent)
-        {
-            this.parent = parent;
-        }
-
-        /// <summary>
-        /// Constructor with parent and children
-        /// </summary>
-        /// <param name="parent">Parent of this object</param>
-        /// <param name="children">List of children to this object</param>
-        public SceneObject(SceneObject parent, List<SceneObject> children)
-        {
-            this.parent = parent;
-            this.children = children;
-        }
-
-        /// <summary>
-        /// Sets a new parent for this object
-        /// </summary>
-        /// <param name="newParent">The new parent</param>
-        public void SetParent(SceneObject newParent)
-        {
-            if(parent != null)
-            {
-                parent.RemoveChild(this);
-            }
-
-            parent = newParent;
-            parent.AddChild(this);
-        }
-
-        /// <summary>
-        /// Adds a child to the game object
-        /// </summary>
-        /// <param name="newChild">Child we want to add</param>
-        public void AddChild(SceneObject newChild)
-        {
-            if(!children.Contains(newChild))
-            {
-                children.Add(newChild);
-            }
-        }
-
-        /// <summary>
-        /// Removes the specified object from the scene
-        /// </summary>
-        /// <param name="child"></param>
-        public void RemoveChild(SceneObject child)
-        {
-            if(children.Contains(child))
-            {
-                children.Remove(child);
-            }
-        }
-
+        { }
     }
 }
