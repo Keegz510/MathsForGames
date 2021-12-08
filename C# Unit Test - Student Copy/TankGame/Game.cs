086ba18e3cabe10676e82d657dcd837246a97ed4
Keegz510 <s200322@students.aie.edu.au>
2021-12-08 14:45:00 +1100

    Added Updating of the scen eobejct and init of the game

--- C# Unit Test - Student Copy/TankGame/Game.cs
+++ C# Unit Test - Student Copy/TankGame/Game.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
 using Raylib_cs;
 using static Raylib_cs.Raylib;
+using MathClasses;
 
 namespace TankGame
 {
@@ -14,18 +14,30 @@ namespace TankGame
         Image dirt = LoadImage("Images/dirt.png");
         Texture2D background;
 
+        PlayerController pc;
+
         public Game()
         {
             background = LoadTextureFromImage(dirt);
+            pc = new PlayerController(new TankObject(new Vector3(320, 475, 1), new Vector3(), -(float)Math.PI / 2, 0, Globals.Scene));
         }
 
         public void Init()
         {
-            
+            Globals.AllObjectsInScene.Add(Globals.Scene);
         }
 
         public void Update()
         {
+            Globals.DeltaTime = GetFrameTime();
+            pc.Input();
+            Globals.Scene.Update();
+            Globals.Scene.PhysicsUpdate();
+
+            Globals.Scene.LocalTransformUpdate();
+            Globals.Scene.GlobalTransformUpdate();
+
+
             Draw();
         }
 
@@ -33,7 +45,8 @@ namespace TankGame
         {
             BeginDrawing();
             ClearBackground(Color.LIGHTGRAY);
-            DrawTextureEx(background, Vector2.Zero, 0, 5, Color.WHITE);
+            DrawTextureEx(background, System.Numerics.Vector2.Zero, 0, 5, Color.WHITE);
+            Globals.Scene.Draw();
             EndDrawing();
         }
     }
