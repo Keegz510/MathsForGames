dea185982c93b4151f3449f7c90217f14a15cd4e
Keegz510 <s200322@students.aie.edu.au>
2021-12-08 13:48:12 +1100

    Added Movement Properties

--- C# Unit Test - Student Copy/TankGame/TankObject.cs
+++ C# Unit Test - Student Copy/TankGame/TankObject.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using MathClasses;
 
 namespace TankGame
 {
@@ -15,5 +18,34 @@ namespace TankGame
         private float DecRate = 25F;                                         //The rate at which the tank decelerates/goes backwards
         private float TurnSpeed = 1F;                                        //The rate at which the tank turns
         private float TurretSpeed = 1F;                                      //The rate at which the turret turns
+
+
+        public Tank(Vector3 position, Vector3 velocity, float rotation, float turretRot, SceneObject parent)
+            : base(position, velocity, rotation, parent)
+        {
+            sprite = LoadTexture("tankBlue_outline.png");
+            offset = new Vector3(-sprite.width / 2, -sprite.height / 2, 0);       //Set the offset as specified
+            //turret = new Turret(new MFG.Vector3(0, 0, 1), 0, sprites, this);        //Assign it a new turret
+            friction = 0.9F;                                                        //Set the friction
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public Vector3 Forward()
+        {
+            return accelaration = Globals.DistanceDirectionToXY(AccRate, GlobalRotation);
+        }
+
+        public Vector3 Backwards()
+        {
+            return accelaration = Globals.DistanceDirectionToXY(DecRate, (GlobalRotation + (float)Math.PI));
+        }
+
+        public float TurnLeft() => RotationShift = -TurnSpeed;
+        public float TurnRight() => RotationShift = TurnSpeed;
+
     }
 }
