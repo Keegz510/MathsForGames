d3e2a817bb222663f6775fa879ea40ee012997ab
Keegz510 <s200322@students.aie.edu.au>
2021-12-08 13:33:44 +1100

    Added movement settings to the object

---
+++ C# Unit Test - Student Copy/TankGame/TankObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame
+{
+    public class TankObject : SceneObject
+    {
+
+
+        /// Movement Settings
+        private float AccRate = 100F;                                        //The rate at which the tank accelerates
+        private float DecRate = 25F;                                         //The rate at which the tank decelerates/goes backwards
+        private float TurnSpeed = 1F;                                        //The rate at which the tank turns
+        private float TurretSpeed = 1F;                                      //The rate at which the turret turns
+    }
+}
